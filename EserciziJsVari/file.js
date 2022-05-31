713d112900a3fd704907328de0ead10b2b766433
domenico <vichidomenico98@hotmail.com>
2022-05-31 10:41:20 +0200

    eventListener

---
+++ EserciziJsVari/file.js
@@ -0,0 +1,15 @@
+//Esercizio 1: creare una calcolatrice per sommare due numeri. 
+
+'use strict';
+
+let n1,n2;
+
+n1 = prompt("inserisci il primo numero");
+n2 = prompt("inserisci il secondo numero");
+
+alert("La somma fa " + String(Number(n1)+Number(n2)));
+
+
+//Esercizio 2
+
+
